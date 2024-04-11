5f8efe0e7d68ac941e166ffd6a974db76da15054
Irina Fileva <irinkaskai@gmail.com>
2024-04-11 17:47:15 +0500

    feat: add solution to task logger

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -174,8 +174,14 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    const arg = args.map((item) => JSON.stringify(item)).join(',');
+    logFunc(`${func.name}(${arg}) starts`);
+    const result = func(...args);
+    logFunc(`${func.name}(${arg}) ends`);
+    return result;
+  };
 }
 
 /**
